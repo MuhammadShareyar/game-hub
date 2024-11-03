ab53b9cbcd136a89d44638574a873c9a3838f829
Muhammad Shareyar <muhammad.shareyar@yassir.com>
2024-11-03 23:18:49 +0500

    sorting by orders

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,21 +1,58 @@
-import { Menu, MenuButton, Button, MenuList, MenuItem } from '@chakra-ui/react';
-import { BsChevronDown } from 'react-icons/bs';
+import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
- return (
-   <Menu>
-     <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-       Sort by: Relevance
-     </MenuButton>
-     <MenuList>
-       <MenuItem>1</MenuItem>
-       <MenuItem>2</MenuItem>
-       <MenuItem>3</MenuItem>
-       <MenuItem>4</MenuItem>
-       <MenuItem>5</MenuItem>
-     </MenuList>
-   </Menu>
- );
+interface Props {
+  onSelectOrder: (order: string) => void;
+  selectedOrder: string | null;
 }
 
-export default SortSelector
+const SortSelector = ({ selectedOrder, onSelectOrder }: Props) => {
+  const sortOrders = [
+    {
+      value: "",
+      label: "Relevance",
+    },
+    {
+      value: "-added",
+      label: "Date added",
+    },
+    {
+      value: "name",
+      label: "Name",
+    },
+    {
+      value: "-released",
+      label: "Release date",
+    },
+    {
+      value: "-metacritic",
+      label: "Popularity",
+    },
+    {
+      value: "-rating",
+      label: "Average rating",
+    },
+  ];
+
+  const currentSortOrder = sortOrders.find((o) => o.value === selectedOrder);
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Sort by : {currentSortOrder?.label || "Relevance"}
+      </MenuButton>
+      <MenuList>
+        {sortOrders.map((o) => (
+          <MenuItem
+            key={o.value}
+            value={o.value}
+            onClick={() => onSelectOrder(o.value)}
+          >
+            {o.label}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
