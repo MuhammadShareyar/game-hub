d8993248608dc6961643095bcefe16ae443d8f53
Muhammad Shareyar <muhammad.shareyar@yassir.com>
2024-11-08 23:42:22 +0500

    remove duplicated interfaces

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -7,13 +7,6 @@ export interface Platform {
   name: string;
   slug: string;
 }
-export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-}
 
 const usePlatforms = () =>
   useQuery({
